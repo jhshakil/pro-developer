cb231dd03190dbe22a22ae241adec245eab002bc
Shakil <shakil11271@gmail.com>
2022-04-17 20:52:03 +0600

    make navbar responsive

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -15,21 +15,41 @@ const Header = () => {
         //         <Link to='/register'>Register</Link>
         //     </div>
         // </div>
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Pro Developer</Navbar.Brand>
+        // <>
+        //     <Navbar bg="dark" variant="dark">
+        //         <Container>
+        //             <Navbar.Brand href="#home">Pro Developer</Navbar.Brand>
+        //             <Nav className="me-auto">
+        //                 <Nav.Link as={Link} to="/home">Home</Nav.Link>
+        //                 <Nav.Link as={Link} to="/about">About</Nav.Link>
+        //                 <Nav.Link href="#contact">Contact</Nav.Link>
+        //                 <Nav.Link href="#orders">Orders</Nav.Link>
+        //                 <Nav.Link href="#login">Log In</Nav.Link>
+        //                 <Nav.Link href="#register">Register</Nav.Link>
+        //             </Nav>
+        //         </Container>
+        //     </Navbar>
+        // </>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
+                        <Navbar.Brand as={Link} to="/home">Pro Developer</Navbar.Brand>
+                    </Nav>
+                    <Nav>
+
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
                         <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
-                        <Nav.Link href="#orders">Orders</Nav.Link>
-                        <Nav.Link href="#login">Log In</Nav.Link>
-                        <Nav.Link href="#register">Register</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                        <Nav.Link as={Link} to="/orders">Orders</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Log In</Nav.Link>
+                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
                     </Nav>
-                </Container>
-            </Navbar>
-        </>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
