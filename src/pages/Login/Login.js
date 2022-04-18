cbefd4eec57735e4f9db2bd21b4115e91455d00c
Shakil <shakil11271@gmail.com>
2022-04-18 06:23:59 +0600

    complete login system with email and password

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,30 +1,45 @@
 import React, { useRef } from 'react';
 import { Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate()
+    if (user) {
+        navigate('/home')
+    }
     const handleSubmit = event => {
-        event.prevendDefault();
+        event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password)
     }
     return (
-        <Form onSubmit={handleSubmit} className='container w-50 mt-5 border border-dark p-5'>
+        <div className='container w-50 mt-5 border border-dark p-5'>
             <h3 className='text-center mb-4'>Log In</h3>
             <button className='w-100 p-2 mb-4'>Login with Google</button>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
-            </Form.Group>
-            <input className='w-100 p-2' type="submit" value="Log In" />
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+                <input className='w-100 p-2' type="submit" value="Log In" />
+            </Form>
             <p className='m-3 text-center'>New to Pro Developer : <Link className='text-decoration-none' to='/register'>Create an Account</Link></p>
-        </Form>
+        </div>
     );
 };
 
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,40 +1,60 @@
 import React, { useRef } from 'react';
-import { Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
+
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const navigate = useNavigate()
+    if (user) {
+        navigate('/home')
+    }
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const nameRef = useRef('');
     const handleSubmit = event => {
-        event.prevendDefault();
+        event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
         const name = nameRef.current.value;
+        createUserWithEmailAndPassword(email, password);
     }
     return (
-        <Form onSubmit={handleSubmit} className='container w-50 mt-5 border border-dark p-5'>
+        <div className='container w-50 mt-5 border border-dark p-5'>
             <h3 className='text-center mb-4'>Register</h3>
             <button className='w-100 p-2 mb-4'>Register with Google</button>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label>Your Name</Form.Label>
-                <Form.Control ref={nameRef} type="name" placeholder="Enter Your Name" required />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
+            <Form onSubmit={handleSubmit} >
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control ref={nameRef} type="name" placeholder="Enter Your Name" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+                {/* <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
-            </Form.Group>
-            <input className='w-100 p-2' type="submit" value="Log In" />
+            </Form.Group> */}
+                {/* <Button className='w-100 p-2'>Register</Button> */}
+                <input className='w-100 p-2' type="submit" value="Register" />
+
+            </Form>
             <p className='m-3 text-center'>Already have an Account : <Link className='text-decoration-none' to='/login'>Log In</Link></p>
-        </Form>
+        </div>
     );
 };
 
